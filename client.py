7be1cff10ba82e933df8196308bb52df52472310
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2024-11-19 20:40:05 +0530

    ENH: Create client-side script
    Implement the client logic including:
      * generating a one-time machine ID
      * reading username and password from STDIN
      * sending the request with the custom auth header and printing the response.

---
+++ client.py
@@ -0,0 +1,45 @@
+import os.path as op
+import sys
+from urllib.parse import urlencode
+import uuid
+
+import requests
+
+
+def get_machine_id():
+    """Get the machine ID for the client. If it doesn't exist, create one."""
+    if not op.exists(".machine_id"):
+        machine_id = str(uuid.uuid4())
+        with open(".machine_id", "w") as f:
+            f.write(machine_id)
+    with open(".machine_id", "r") as f:
+        machine_id = f.read()
+    return machine_id
+
+
+def run(url):
+    """Entry point for the client.
+
+    Reads the username and password from STDIN, and sends a GET request to the server.
+    """
+    data = sys.stdin.read()
+    try:
+        username, password = data.splitlines()
+    except ValueError:
+        raise ValueError(
+            (
+                "Invalid input: This client expects exactly two lines from STDIN.",
+                "Ensure that username and password are present in two lines,",
+                "in the following format:\n '{username}\\n{password}'",
+            )
+        )
+    machine_id = get_machine_id()
+    auth_header = urlencode(
+        {"username": username, "password": password, "machine_id": machine_id}
+    )
+    response = requests.get(url, headers={"SEEK_CUSTOM_AUTH": auth_header})
+    print(response.json())
+
+
+if __name__ == "__main__":
+    run()
